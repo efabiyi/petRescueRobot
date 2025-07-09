092c34c0f48dedc843654db14ba49bb6bd75add3
tumi! <94874741+efabiyi@users.noreply.github.com>
2025-07-09 15:57:54 -0700

    Merging me and grace's repos. Need to do clean up but it builds!

--- pawPatrol/firmware/src/main.cpp
+++ pawPatrol/firmware/src/main.cpp
@@ -3,6 +3,9 @@
 #include "secrets.h"
 #include "logger.h"
 #include "wifiManager.h"
+#include "drive.h"
+#include "detect.h"
+
 
 HallSensor hallSensor(32); // GPIO pin for the hall sensor
 WifiManager wifiMgr;
@@ -10,15 +13,19 @@ WifiManager wifiMgr;
 void setup() {
   Serial.begin(115200);
   wifiMgr.startWifi();
+  initialize_drive();
 }
 
 void loop() {
+
+  drive();
 
   float hallVoltage = hallSensor.readVoltage();
   bool magnetDetected = hallSensor.detectMagnet(hallVoltage);
   String hallData = "Hall Sensor Voltage: " + String(hallVoltage, 2) + " V, Magnet Detected: " + (magnetDetected ? "Yes" : "No");
   Logger::send(hallData);
 
+  
   Serial.println(hallData);
   delay(100);
 }
