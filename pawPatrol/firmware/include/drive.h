d70462044c9d6092ee681763bed0e6256eb47eef
Grace <gracewang604@gmail.com>
2025-07-10 12:07:38 -0700

    Update drive.h

--- pawPatrol/firmware/include/drive.h
+++ pawPatrol/firmware/include/drive.h
@@ -16,20 +16,20 @@ extern const int FWD_LEFT_PWM;
 extern const int BWD_LEFT_PWM;
 extern const int FWD_RIGHT_PWM;
 extern const int BWD_RIGHT_PWM;
- extern const int FWD_LEFT_CHAN;
+extern const int FWD_LEFT_CHAN;
 extern const int BWD_LEFT_CHAN;
 extern const int FWD_RIGHT_CHAN;
 extern const int BWD_RIGHT_CHAN;
 
 // Function declarations
-void initialize_drive();
-void left_drive_forward(int speed);
-void left_drive_backward(int speed);
-void right_drive_forward(int speed);
-void right_drive_backward(int speed);
-float calculate_error();
+void initializeDrive();
+void leftDriveForward(int speed);
+void leftDriveBackward(int speed);
+void rightDriveForward(int speed);
+void rightDriveBackward(int speed);
+float calculateError();
 void drive();
-void test_drive(int left_speed, int right_speed);
-void test_drive_bwd(int left_speed, int right_speed);
+void testDrive(int leftSpeed, int rightSpeed);
+void testDriveBwd(int leftSpeed, int rightSpeed);
 
 #endif
